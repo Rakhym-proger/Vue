eee995582cc1691cf505605c63f3a9b1ef212e80
Rakhym-proger <56889013+Rakhym-proger@users.noreply.github.com>
2021-04-12 14:18:25 +0600

    first1

--- src/main.js
+++ src/main.js
@@ -3,15 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from "@/router";
 import store from './store'
-import { sync } from 'vuex-router-sync'
 
 
 Vue.config.productionTip = false
-const unsync = sync(store, router)
 new Vue({
   vuetify,
   router,
   store,
-  unsync,
   render: h => h(App)
 }).$mount('#app')
